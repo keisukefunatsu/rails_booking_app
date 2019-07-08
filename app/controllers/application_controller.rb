f1c6e49eaef5497375bd7d703bfaaa6cdaf00c94
funatsu <funatsu0813@gmail.com>
2019-07-08 16:42:05 +0000

    improve token auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,9 @@ class ApplicationController < ActionController::API
         decoded_token = JWT.decode(sent_token, Rails.application.credentials.secret_key_base)[0]
         user_id = decoded_token['user_id']
         expire_at = decoded_token['expire_at']
-        if expire_at > Time.now
-          @current_user = User.find(user_id)
-        else
-          raise 'token expired'
-        end
+        @current_user = User.find(user_id)
+        raise 'token is old' unless @current_user.auth_token == sent_token
+        raise 'token expired' unless expire_at > Time.now 
       rescue
         render status: 403, json: 
           {
--- spec/requests/auth_spec.rb
+++ spec/requests/auth_spec.rb
@@ -7,16 +7,32 @@ describe "Login with post data", :type => :request do
     expect(auth_token).not_to eq(nil)
   end
   it 'can get auth_token and use it to access controllers' do
-    get '/users.json', headers: {Authorization: auth_token}
+    get '/users.json', headers: { Authorization: auth_token }
     expect(response).to have_http_status(:success)
   end
   
   it 'fails to access with expired token' do
     travel_to 25.hours.from_now do
-      get '/users.json', headers: {Authorization: auth_token}
+      get '/users.json', headers: { Authorization: auth_token }
       expect(response).to have_http_status(:forbidden)
     end
   end
+  
+  it 'fails to access with old token' do
+      # to get different token
+      sleep 1.second 
+      post '/authenticate', params: {email: login_user.email, password: login_user.password} 
+      get '/users.json', headers: { Authorization: auth_token }
+      expect(response).to have_http_status(:forbidden)
+  end
+  
+  it 'can access with renewed token' do 
+    post '/authenticate', params: {email: login_user.email, password: login_user.password} 
+    renewed_auth_token = JSON.parse(response.body)['auth_token'] 
+    get '/users.json', headers: { Authorization: renewed_auth_token }
+    expect(response).to have_http_status(:success)
+  end
+  
   
   it 'fails to access controllers without Authorization header' do
     get '/users.json'
